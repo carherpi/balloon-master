6cd03f090db803fd2b20c182a3c08593bda5f3fc
carherpi <carloshpiedra@gmail.com>
2021-12-10 12:50:54 +0100

    Improved balloon movement

---
+++ Assets/Scripts/Movement/BalloonJelly.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonJelly : MonoBehaviour
+{
+    public float Intensity = 1f;
+    public float Mass = 1f;
+    public float stiffness = 1f;
+    public float damping = 0.75f;
+
+    private Mesh OriginalMesh, MeshClone;
+    private MeshRenderer renderer;
+    private JellyVertex[] jv;
+    private Vector3[] vertexArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OriginalMesh = GetComponent<MeshFilter>().sharedMesh;
+        MeshClone = Instantiate(OriginalMesh);
+        GetComponent<MeshFilter>().sharedMesh = MeshClone;
+        renderer = GetComponent<MeshRenderer>();
+        jv = new JellyVertex[MeshClone.vertices.Length];
+        for (int i = 0; i < MeshClone.vertices.Length; i++)
+        {
+            jv[i] = new JellyVertex(i, transform.TransformPoint(MeshClone.vertices[i]));
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        vertexArray = OriginalMesh.vertices;
+        for (int i = 0; i < jv.Length; i++)
+        {
+            Vector3 target = transform.TransformPoint(vertexArray[jv[i].ID]);
+            float intensity = (1 - (renderer.bounds.max.y - target.y) / renderer.bounds.size.y) * Intensity;
+            jv[i].Shake(target, Mass, stiffness, damping);
+            target = transform.InverseTransformPoint(jv[i].Position);
+            vertexArray[jv[i].ID] = Vector3.Lerp(vertexArray[jv[i].ID], target, intensity);
+
+        }
+        MeshClone.vertices = vertexArray;
+    }
+
+    public class JellyVertex
+    {
+        public int ID;
+        public Vector3 Position;
+        public Vector3 velocity, Force;
+
+        public JellyVertex(int _id, Vector3 _pos)
+        {
+            ID = _id;
+            Position = _pos;
+        }
+
+        public void Shake(Vector3 target, float m, float s, float d)
+        {
+            Force = (target - Position) * s;
+            velocity = (velocity + Force / m) * d;
+            Position += velocity;
+            if ((velocity + Force + Force / m).magnitude < 0.001f)
+            {
+                Position = target;
+            }
+        }
+
+    }
+}
--- Assets/Scripts/Movement/BalloonMovement.cs
+++ Assets/Scripts/Movement/BalloonMovement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
+
 
 public class BalloonMovement : MonoBehaviour
 {
@@ -10,18 +10,37 @@ public class BalloonMovement : MonoBehaviour
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask groundMask;
 
-    private Vector3 velocity;
 
     public GameAutomaton GameAutomatonScript;
-
-    Vector3 oldPosition;
     Vector3 desiredPosition;
+    ConstantForce desiredForce;
 
-    Rigidbody rbody;
+    
 
-    bool collision = false;
+    public float maxSpeed;
+    public float maxHeight;
 
-    public float startSpeed;
+    private float speed;
+    private float offset;
+    private float n;
+    private float slowDownSpeed;
+    private float rotDirection;
+
+    private float desiredRot;
+    public float rotSpeed = 5;
+    public float damping = 10;
+    public Quaternion desiredRotQ;
+
+    public enum BalloonStates
+    {
+        GoingUpFast,
+        GoingUpSlow,
+        Floating,
+        GoingDown,
+    }
+
+    private BalloonStates ballonState;
+
 
     private void Awake()
     {
@@ -31,60 +50,171 @@ public class BalloonMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.gameObject.GetComponent<ConstantForce>().enabled = false;
-        rbody = this.gameObject.GetComponent<Rigidbody>();
 
-        desiredPosition = transform.position + new Vector3(0, -transform.position.y, 0);
+        ballonState = BalloonStates.GoingDown;
 
-        oldPosition = transform.position;
+        desiredForce = this.gameObject.GetComponent<ConstantForce>();
+        desiredForce.enabled = false;     
+
+        desiredPosition = transform.position;
+        desiredPosition.y = 0;
+        speed = maxSpeed;
+
+        slowDownSpeed = 0.1f;
+
+        // rot
+        desiredRot = transform.eulerAngles.z;
+        desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, desiredRot);
+
     }
 
-    void Update()
-    {
-        
-        
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
 
-        if (transform.position != desiredPosition)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * 1);
-        }
-
-        Movement();
-    }
-
-    private void Movement()
-    {
         if ((int)GameAutomatonScript.GetGameState() != 3) // if GameRunning
         {
             return;
-            
         }
 
-        // Activate ct force
-        this.gameObject.GetComponent<ConstantForce>().enabled = true;
+        transform.Rotate(new Vector3(rotDirection * 0.1f, rotDirection *  0.1f, rotDirection * 0.3f));
+
+
+        if (ballonState == BalloonStates.GoingUpFast || ballonState == BalloonStates.GoingUpSlow)
+        {
+            goUp();
+            //Shake();
+        } 
+
+        if (ballonState == BalloonStates.Floating)
+        {
+            goFloating();
+        }
+
+        if (ballonState == BalloonStates.GoingDown)
+        {
+            goDown();
+        }
+        
+        // activate force
+        //desiredForce.enabled = true;
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        n = Random.Range(-1.0f, 1.0f);
+        if (n > 0)
+        {
+            offset = Random.Range(-1.5f, -0.5f);
+            rotDirection = -1.0f;
+        }
+        else
+        {
+            offset = Random.Range(0.5f, 1.5f);
+            rotDirection = 1.0f;
+        }
+
+        if (collision.gameObject.name.Contains("Boy"))
+        {
+            FastBounce();
+
+        } else // touch floor or anything else
+        {
+
+            SlowBounce();            
+        }
+    }
+
+
+    public void FastBounce()
+    {
+
+        ballonState = BalloonStates.GoingUpFast;
+
+        // update desired position & speed
+        desiredPosition.y = maxHeight;
+
+        Debug.Log("Offset!");
+        Debug.Log(offset);
+
+        desiredPosition.x = transform.position.x + offset;
+        desiredPosition.z = transform.position.z + offset;
+        speed = 3.0f;        
+    }
+
+    public void SlowBounce()
+    {
+
+        ballonState = BalloonStates.GoingUpSlow;
+
+        // update desired position & speed
+        desiredPosition.y = 2;
+        speed = 0.3f;        
+    }
+
+    public void goUp()
+    {
+        
+        // check if it has arrived desired position
+        if (transform.position.y < maxHeight & speed >= 0) {
+            transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * speed);
+            speed -= slowDownSpeed; // slows down the balloon rapidly
+            //slowDownSpeed += 0.1f;
+        } else
+        {
+            ballonState = BalloonStates.Floating;
+            speed = 0.0f;
+        }
+    }
+
+    public void goFloating()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * speed);
+        
+        if (n > 0)
+        {
+            desiredPosition.x += 0.1f;
+            desiredPosition.z += 0.1f;
+        } else
+        {
+            desiredPosition.x -= 0.1f;
+            desiredPosition.z -= 0.1f;
+        }
+        
+        speed += 0.01f;
+
+        if (speed >= maxSpeed)
+        {
+            ballonState = BalloonStates.GoingDown;
+            desiredPosition.y = 0;
+
+            if (n > 0)
+            {
+                
+                desiredPosition.x = transform.position.x + 1f;
+                desiredPosition.z = transform.position.z + 1f;
+            }
+            else
+            {
+                desiredPosition.x = transform.position.x - 1f;
+                desiredPosition.z = transform.position.z - 1f;
+            }
+            
+        }
+    }
+
+    public void goDown()
+    {
+        
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * speed);        
 
     }
 
-    void OnCollisionEnter()
+    public void Shake()
     {
-        /*
-        if (other.gameObject.name == "gameobject2")
-            rigidbody.AddForce(Vector3.Up * force);
-        */
-        Debug.Log("Bounce!");
-
-        //this.gameObject.GetComponent<ConstantForce>().force = new Vector3(0f, 1f, 0f);       
-
-        transform.position = transform.position + new Vector3(0, 3, 0);
-        rbody.drag = 20;
-
-        //desiredPosition = transform.position + new Vector3(0, 1, 0);
-
-
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Time.deltaTime * damping);
     }
 }
