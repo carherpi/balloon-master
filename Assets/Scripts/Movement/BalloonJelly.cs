2f2786440acba22f4097cefd7d006ae8b77c2114
Marc von Mirbach <marc.mirbach@rwth-aachen.de>
2021-12-15 21:15:59 +0100

    MM/Remove repeated imports

--- Assets/Scripts/Movement/BalloonJelly.cs
+++ Assets/Scripts/Movement/BalloonJelly.cs
@@ -2,10 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class BalloonJelly : MonoBehaviour
 {
     public float Intensity = 1f;
