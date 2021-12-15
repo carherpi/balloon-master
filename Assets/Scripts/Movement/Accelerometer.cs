5e56f4dea209b79738d4d7c565eca1dacefb0a1f
carherpi <carloshpiedra@gmail.com>
2021-12-15 19:59:41 +0100

    Fix for when the child jumps and applies a huge force to the balloon

--- Assets/Scripts/Movement/Accelerometer.cs
+++ Assets/Scripts/Movement/Accelerometer.cs
@@ -16,17 +16,17 @@ public class Accelerometer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Fix for when the child jumps and applies a huge force to the balloon
+        if (rbBalloon.velocity.y > 0)
+        {
+            Debug.Log(rbBalloon.velocity);
+            rbBalloon.velocity = new Vector3(0f, 0f, 0f);
+        } 
     }
 
     public Vector3 CalculateDirection()
     {
         Vector3 tilt = Input.acceleration;
-        Debug.Log(tilt);
-        //tilt = Quaternion.Euler(90, 0, 0) * tilt;
-        //Debug.Log(tilt);
-        //rbBalloon.AddForce(tilt * speed);
-
         return tilt;
     }
 }
