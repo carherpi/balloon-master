ae47e48d05c2d51f4f3ce3c9a6d11d863c7dc6cf
carherpi <carloshpiedra@gmail.com>
2022-01-04 18:00:43 +0100

    Edited SceneSelectCharacter

---
+++ Assets/Scripts/MainMenu/CharacterSelection.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+	public GameObject[] characters;
+	public int selectedCharacter = 0;
+
+	public void Start()
+	{
+		foreach (GameObject gameObject in characters)
+		{
+			gameObject.SetActive(false);
+		}
+		characters[0].SetActive(true);
+	}
+
+	public void NextCharacter()
+	{
+		characters[selectedCharacter].SetActive(false);
+		selectedCharacter = (selectedCharacter + 1) % characters.Length;
+		characters[selectedCharacter].SetActive(true);
+	}
+
+	public void PreviousCharacter()
+	{
+		characters[selectedCharacter].SetActive(false);
+		selectedCharacter--;
+		if (selectedCharacter < 0)
+		{
+			selectedCharacter += characters.Length;
+		}
+		characters[selectedCharacter].SetActive(true);
+	}
+
+	public void StartGame()
+	{
+		//PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
+		//SceneManager.LoadScene(1, LoadSceneMode.Single);
+	}
+}
