46f97c37f5d874549f220bdf1ab596b95d79f6bf
Marc von Mirbach <marc.mirbach@rwth-aachen.de>
2022-01-19 18:58:21 +0100

    Hopefully a workaround for controls

--- Assets/Scripts/Movement/GyroscopeHandler.cs
+++ Assets/Scripts/Movement/GyroscopeHandler.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class GyroscopeHandler : MonoBehaviour
 {
+    private Compass compass;
     private Gyroscope gyro;
     private Quaternion initOrient;
     private Quaternion initOrientSubtract;
+    private Vector3 initOrientEuler;
+    private Vector3 initGravity;
     private Vector2 inputMovement;
     private readonly float scaleAxis = 0.1f;
     private bool calibrated;
 
     void Awake()
     {
+        this.compass = Input.compass;
+        this.compass.enabled = true;
+        Debug.Log("Compass enabled");
         this.gyro = Input.gyro;
         this.gyro.enabled = true;
         Debug.Log("Gyro enabled");
@@ -21,8 +27,24 @@ public class GyroscopeHandler : MonoBehaviour
     /** return: true if calibration successful; false if calibration needs to be repeated after a while */
     public bool Calibrate()
     {
+        if (SystemInfo.supportsGyroscope)
+        {
+            Debug.LogError("Your device does not have a gyroscope.");
+            // return false;
+        }
+        if (SystemInfo.supportsAccelerometer)
+        {
+            Debug.LogError("Your device does not have an accelerometer.");
+            // return false;
+        }
+        Debug.Log("Compass trueHeading" + this.compass.trueHeading);
+        Debug.Log("Compass magneticHeading" + this.compass.magneticHeading);
+        Debug.Log("Compass rawVector" + this.compass.rawVector);
+
         this.initOrient = this.gyro.attitude;
+        this.initGravity = this.gyro.gravity;
         this.initOrientSubtract = Quaternion.Inverse(this.initOrient);
+        this.initOrientEuler = this.initOrient.eulerAngles;
         Debug.Log("Initial Orientation: " + this.initOrient);
         this.calibrated = this.initOrient != Quaternion.identity;
         return calibrated;
@@ -36,14 +58,29 @@ public class GyroscopeHandler : MonoBehaviour
         }
 
         Quaternion diffVector = this.gyro.attitude * this.initOrientSubtract;
+        Vector3 diffVectorEuler = this.gyro.attitude.eulerAngles - this.initOrientEuler;
+        Vector3 diffVectorGravity = this.gyro.gravity - this.initGravity;
         //Debug.Log("Enabled: " + this.gyro.enabled);
-        //Debug.Log(this.gyro.attitude);
-        //Debug.Log("InputMovement: " + diffVector.eulerAngles);
+        Debug.Log("GyroAttitude        " + this.gyro.attitude.eulerAngles);
+        Debug.Log("RotationRate        " + this.gyro.rotationRate);
+        Debug.Log("RotRateUnbia        " + this.gyro.rotationRateUnbiased);
+        Debug.Log("GyroGravity         " + this.gyro.gravity);
+        Debug.Log("UserAccelera        " + this.gyro.userAcceleration);
+        //Debug.Log("InputMovementQuate: " + diffVector.eulerAngles);
+        //Debug.Log("InputMovementEuler: " + diffVectorEuler);
+        Debug.Log("InputMovementGravity: " + diffVectorGravity);
+        //Debug.Log("* (0,0,1,0)         " + this.gyro.attitude * new Quaternion(0, 0, 1, 0));
+        //Debug.Log("* (0,0,1,0)         " + (this.gyro.attitude * new Quaternion(0, 0, 1, 0)).eulerAngles);
 
         // adjust values
-        float xMove = AdjustValues(diffVector.eulerAngles.x);
-        float yMove = AdjustValues(diffVector.eulerAngles.y);
+        //float xMove = AdjustValues(diffVector.eulerAngles.x);
+        //float yMove = AdjustValues(diffVector.eulerAngles.y);
+        //Vector2 move = new Vector2(xMove, yMove);
+
+        float xMove = AdjustValues(diffVectorGravity.x);
+        float yMove = AdjustValues(diffVectorGravity.y);
         Vector2 move = new Vector2(xMove, yMove);
+
         // limit maximal movement to 1
         if (move.magnitude > 1)
         {
@@ -57,14 +94,25 @@ public class GyroscopeHandler : MonoBehaviour
     private float AdjustValues(float axisMove)
     {
         // handle overflow
-        if (axisMove > 180)
-        {
-            axisMove -= 360;
-        }
+        //if (axisMove > 180)
+        //{
+        //    axisMove -= 360;
+        //}
         // change direction
-        axisMove *= -1;
+        //axisMove *= -1;
         // scale movement
-        axisMove *= this.scaleAxis;
+        axisMove *= 4; // this.scaleAxis;
         return axisMove;
     }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        Debug.Log("Original2: " + q);
+        Quaternion gyroQuaternion = new Quaternion(q.x, q.y, -q.z, -q.w);
+        Debug.Log("Original3: " + gyroQuaternion);
+        gyroQuaternion = Quaternion.Euler(90f, 0f, 0f) * gyroQuaternion;
+        Debug.Log("Original4: " + gyroQuaternion);
+        return gyroQuaternion;
+    }
+
 }
